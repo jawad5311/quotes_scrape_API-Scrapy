0ea604da52f810e81a1b83d2803e4788714b36ce
Jawad <muhammadjawad5311@gmail.com>
2021-06-05 12:25:52 +0500

    Data scrapped & added to SQLlite3 database

--- quotes_to_scrape_API/quotes_to_scrape_API/pipelines.py
+++ quotes_to_scrape_API/quotes_to_scrape_API/pipelines.py
@@ -6,8 +6,34 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import sqlite3
 
 
-class QuotesToScrapeApiPipeline:
+class SQLlitePipeline:
+    def open_spider(self, spider):
+        self.connection = sqlite3.Connection("quotes_API.db")
+        self.c = self.connection.cursor()
+        try:
+            self.c.execute("""
+                CREATE TABLE quotes_scrapped_using_api(
+                    author TEXT,
+                    tags LIST,
+                    quote TEXT
+                    )    
+            """)
+            self.connection.commit()
+        except sqlite3.OperationalError:
+            pass
+
     def process_item(self, item, spider):
-        return item
+        self.c.execute("""
+                    INSERT INTO quotes_scrapped_using_api (author,tags,quote) VALUES(?,?,?)
+                """, (
+            item.get('author'),
+            str(item.get('tags')),
+            item.get('quote')
+        ))
+        self.connection.commit()
+
+    def close_spider(self, spider):
+        self.connection.close()
